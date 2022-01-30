c579afbf3c3940d79920a9874434ac3d78986e8c
ArifulIslam99 <afridi2000rampura@gmail.com>
2022-01-30 22:41:08 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,73 @@
+
+
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 5000;
+const { MongoClient } = require('mongodb');
+require('dotenv').config();
+const cors = require('cors');
+app.use(cors())
+
+const fileUpload = require('express-fileupload');
+const { query } = require('express');
+app.use(fileUpload())
+
+const uri = "mongodb+srv://xcit_education:DXtdUlkqCMJs64NB@cluster0.xbjez.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
+async function run(){
+
+    try{
+
+
+            await client.connect()
+            const database = client.db('Xcit_Education');
+            const blogCollection = database.collection('blogCollection');
+
+            app.get('/blogs', async(req, res)=>{
+                const blogs =  blogCollection.find({});
+                const result = await blogs.toArray();
+                res.json(result)
+            })
+
+
+            app.post('/blogs', async(req, res )=>{
+                const title = req.body.title;
+                const author = req.body.author;
+                const description = req.body.description;
+                const status = req.body.status;
+                const date = req.body.date
+                const pic = req.files.image;
+                const picData = pic.data;
+                const encodedPic = picData.toString('base64')
+                const image = Buffer.from(encodedPic, 'base64');
+                
+                 const blogs = {
+                   
+                    title, date, description, author, status, image,
+                 }
+
+                  console.log(blogs)
+      
+                 const result  = await blogCollection.insertOne(blogs)
+                 res.json(result)
+              }) 
+      
+
+
+    }
+    finally{
+
+    }
+
+}
+
+run().catch(console.dir)
+
+app.get('/', (req, res)=>{
+    res.send('Xcit Running')
+})
+
+app.listen(port, ()=>{
+    console.log('Running port', port)
+})
